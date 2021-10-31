e937b4074fcb2373ac6ea8482b310cbe84f63d2d
Daniil <phpinfo12345@gmail.com>
2021-10-31 19:25:39 +0300

    change namespace & use generic host

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace T2DUploader
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {            
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // services.AddHostedService<ConsoleHostedService>();
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
