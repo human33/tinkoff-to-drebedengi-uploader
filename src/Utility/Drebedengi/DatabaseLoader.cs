39b21b3cdcb23b547ef0f1f201ba06f8df8612d2
Daniil <phpinfo12345@gmail.com>
2023-01-28 13:44:07 +0300

    add a file

---
+++ src/Utility/Drebedengi/DatabaseLoader.cs
@@ -0,0 +1,14 @@
+namespace T2DUploader.Utility.Drebedengi
+{
+    class DatabaseLoader
+    {   
+        // todo: do something like that
+        // public Database Load(string drebedengiPassword) 
+        // {
+        //     string url = "https://www.drebedengi.ru/?module=v2_homeBuhPrivateExport";
+        //     string requestBody = $"action=do_archive&password={drebedengiPassword}&exportType=1&is_sent_backup=false";
+
+        // }
+
+    }
+}
